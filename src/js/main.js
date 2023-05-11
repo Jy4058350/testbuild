ddc3098d5acba02bdf8a0d81d94af9f5938f0344
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-05-11 10:07:23 +0900

    parcelsetting

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import Parallax from 'parallax-js';
+
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true
+});
+
+parallaxInstance.friction(0.2, 0.2);
